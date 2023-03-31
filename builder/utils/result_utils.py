0c5263f74b4ab7012616e6b60fd9f8bd4e8f0c72
rhksgud92 <martin9209@naver.com>
2023-03-31 20:48:56 +0900

    seedcase

--- builder/utils/result_utils.py
+++ builder/utils/result_utils.py
@@ -22,27 +22,15 @@ class experiment_results_validation:
         os.system("echo  \'#######################################\'")
         seed, result = list_of_test_results_per_seed
 
-        if (self.args.model_types == "detection"):
-            os.system("echo  \'##### validation results #####\'")
-            if "rmse" in self.args.auxiliary_loss_type:
-                auc, apr, f1, rmse = result
-                os.system("echo  \'auc: {}, apr: {}, f1_score: {}, rmse: {}\'".format(str(auc), str(apr), str(f1), str(rmse)))
-                self.rmse_final_list.append(rmse)
-            else:
-                auc, apr, f1 = result
-                os.system("echo  \'seed_case:{} - auc: {}, apr: {}, f1_score: {}\'".format(str(seed), str(auc), str(apr), str(f1)))
+        os.system("echo  \'##### validation results #####\'")
+        if "rmse" in self.args.auxiliary_loss_type:
+            auc, apr, f1, rmse = result
+            os.system("echo  \'seed_case:{} - auc: {}, apr: {}, f1_score: {}, rmse: {}\'".format(str(seed), str(auc), str(apr), str(f1), str(rmse)))
+            self.rmse_final_list.append(rmse)
+        else:
+            auc, apr, f1 = result
+            os.system("echo  \'seed_case:{} - auc: {}, apr: {}, f1_score: {}\'".format(str(seed), str(auc), str(apr), str(f1)))
             
-        elif (self.args.model_types == "classification"):
-            nresult, wresult = result
-            aucs, aprs, f1s = nresult
-            auc, apr, f1 = wresult
-            os.system("echo  \'##### validation results #####\'")
-            os.system("echo  \'seed_case:{} - weighted auc: {}, apr: {}, f1: {}\'".format(str(seed), str(auc), str(apr), str(f1)))
-            os.system("echo  \'Each range auc: {}, \n Each range apr: {}, \n Each range f1: {}\'".format(str(aucs), str(aprs), str(f1s)))        
-            self.aucs_final_list.append(aucs)
-            self.aprs_final_list.append(aprs)
-            self.f1s_final_list.append(f1s)
-        
         self.auc_final_list.append(auc)
         self.apr_final_list.append(apr)
         self.f1_final_list.append(f1)
@@ -95,26 +83,14 @@ class experiment_results_test:
         os.system("echo  \'#######################################\'")
         seed, result = list_of_test_results_per_seed
 
-        if (self.args.model_types == "detection"):
-            os.system("echo  \'##### test results #####\'")
-            if "rmse" in self.args.auxiliary_loss_type:
-                auc, apr, f1, rmse = result
-                os.system("echo  \'auc: {}, apr: {}, f1_score: {}, rmse: {}\'".format(str(auc), str(apr), str(f1), str(rmse)))
-                self.rmse_final_list.append(rmse)
-            else:
-                auc, apr, f1 = result
-                os.system("echo  \'seed_case:{} - auc: {}, apr: {}, f1_score: {}\'".format(str(seed), str(auc), str(apr), str(f1)))
-            
-        elif (self.args.model_types == "classification"):
-            nresult, wresult = result
-            aucs, aprs, f1s = nresult
-            auc, apr, f1 = wresult
-            os.system("echo  \'##### test results #####\'")
-            os.system("echo  \'seed_case:{} - weighted auc: {}, apr: {}, f1: {}\'".format(str(seed), str(auc), str(apr), str(f1)))
-            os.system("echo  \'Each range auc: {}, \n Each range apr: {}, \n Each range f1: {}\'".format(str(aucs), str(aprs), str(f1s)))        
-            self.aucs_final_list.append(aucs)
-            self.aprs_final_list.append(aprs)
-            self.f1s_final_list.append(f1s)
+        os.system("echo  \'##### test results #####\'")
+        if "rmse" in self.args.auxiliary_loss_type:
+            auc, apr, f1, rmse = result
+            os.system("echo  \'seed_case:{} - auc: {}, apr: {}, f1_score: {}, rmse: {}\'".format(str(seed), str(auc), str(apr), str(f1), str(rmse)))
+            self.rmse_final_list.append(rmse)
+        else:
+            auc, apr, f1 = result
+            os.system("echo  \'seed_case:{} - auc: {}, apr: {}, f1_score: {}\'".format(str(seed), str(auc), str(apr), str(f1)))
         
         self.auc_final_list.append(auc)
         self.apr_final_list.append(apr)
